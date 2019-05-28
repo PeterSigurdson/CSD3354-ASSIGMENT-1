5058e24f123766a1e0460793a509af4f8d8623cb
Peter Sigurdson <peter@petersigurdson.net>
2019-05-28 13:02:53 -0400

    initial commit

---
+++ C0949494-C0949493/C0949494-C0949493/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C0949494_C0949493
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
