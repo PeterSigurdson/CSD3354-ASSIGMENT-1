a781df6f9057a29a47c300da3f75b45f0965252b
Peter Sigurdson <peter@petersigurdson.net>
2019-05-28 16:17:21 -0400

    final commit

--- C0949494-C0949493/C0949494-C0949493/Program.cs
+++ C0949494-C0949493/C0949494-C0949493/Program.cs
@@ -50,13 +50,38 @@ namespace C0949494_C0949493
         public void Run()
         {
             int number1, number2;
-
+            int mathOperation;
             Console.WriteLine("enter a number");
             number1 = Convert.ToInt32(Console.ReadLine());
             Console.WriteLine("enter a number");
             number2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("the sum of {0}, and {1}, is {2} ", number1, number2, 0);
 
+            Console.WriteLine("What Math Operation would you like to do:    ");
+            Console.WriteLine("Type 1 for ADD, 2 for Subtract, 3 for Multiply, 4 for Divide: ");
+
+            mathOperation = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("the {0} of {1}, and {2}, is {3} ", "result", number1, number2, process(number1, number2));
+        }
+
+        public int process(int num1, int numb2)
+        {
+            return num1 - numb2;
+        }
+
+        public void SelectOperation(string whatDoesUserWantToDo)
+        {
+            switch (whatDoesUserWantToDo)
+            {
+                //case constant - expression1:
+                //    statement(s);
+                //    break;
+                //case constant - expression2:
+                //case constant - expression3:
+                //    statement(s);
+                //    break;
+
+            }
         }
     }
 }
